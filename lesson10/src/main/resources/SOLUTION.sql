3f61d46b389f4b1773777092af1c85510a962bb6
JustDevWeb <zm91672@gmail.com>
2023-10-27 17:29:06 +0500

    fix errors

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT s.* FROM subject s LEFT JOIN (SELECT subject_id, AVG(mark) as avg_mark FROM Mark GROUP BY subject_id) avg_marks_per_subject ON s.id = avg_marks_per_subject.subject_id WHERE avg_marks_per_subject.avg_mark > (SELECT AVG(mark) FROM Mark);
-SELECT s.* FROM student s LEFT JOIN PAYMENT p ON s.id = p.student_id GROUP BY s.id HAVING SUM(p.AMOUNT) > (SELECT AVG(AMOUNT) FROM PAYMENT);
+SELECT s.* FROM student s LEFT JOIN ( SELECT DISTINCT STUDENT_ID, SUM(p.AMOUNT) AS sum_perstudent FROM PAYMENT p GROUP BY STUDENT_ID ) pmnt ON s.id = pmnt.STUDENT_ID WHERE pmnt.sum_perstudent > ( SELECT AVG(avg_sum_perstudent) FROM ( SELECT SUM(p.AMOUNT) AS avg_sum_perstudent  FROM PAYMENT p  GROUP BY STUDENT_ID  ) avg_pmnt ) ORDER BY pmnt.sum_perstudent DESC;
 
 
--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,3 +4,4 @@ select * from STUDENT where GROUPNUMBER = 10 AND BIRTHDAY >= DATEADD(year, -20,'
 select * from STUDENT where NAME = 'Mike' OR GROUPNUMBER IN (4,5,6);
 select * from PAYMENT where PAYMENT_DATE > DATEADD(month ,-8,'20231014');
 select * from STUDENT where NAME like 'A%';
+select * from STUDENT where (name = 'Roxi' and groupnumber = 4) or (name = 'Tallie' and groupnumber = 9);
